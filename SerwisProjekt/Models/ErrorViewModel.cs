1cb4afda5665ce8ee6eb846edd55097c5d2ec8b0
Karol Prus <karlos004@o2.pl>
2020-04-09 09:25:36 +0200

    init

---
+++ SerwisProjekt/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SerwisProjekt.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
