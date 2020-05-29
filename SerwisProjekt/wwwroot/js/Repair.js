b06736dc0add9ce591f2da1654caa4bff5aae679
Karol Prus <karolbosiak004@gmail.com>
2020-05-29 11:29:05 +0200

    added repair view

---
+++ SerwisProjekt/wwwroot/js/Repair.js
@@ -0,0 +1,33 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tbData').DataTable({
+        "ajax": {
+            "url": "/Admin/Repair/GetAll"
+        },
+        "columns": [
+            { "data": "id", "width": "20%" },
+            { "data": "vin", "width": "20%" },
+            { "data": "description", "width": "30%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                    <div class="text-center">
+    <a href="/Admin/Repair/Upsert/${data}" class="btn btn-succsess text-white">
+        <i class="fas fa-trash-alt"></i>
+    </a>
+    <a href="#" class="btn btn-danger text-white" style="cursor: pointer">
+        <i class="fas fa-trash-alt"></i>
+    </a>
+</div>
+                            `;
+                }, "width": "30%"
+            },
+        ]
+    });
+}
